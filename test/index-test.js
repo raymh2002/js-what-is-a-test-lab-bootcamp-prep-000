01292b363a93af6b7d83ff3340eba598c394337b
Raymond Harvey <raymh2002@aol.com>
2018-01-16 20:22:05 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -14,7 +14,7 @@ describe('what-is-a-test', () => {
     })
   })
 
-  describe('message', () => {
+  describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
